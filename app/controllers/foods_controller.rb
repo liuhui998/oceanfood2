587601451721d4355db0bd2d09769ba72065e91d
Peter.Liu <liuhui998@gmail.com>
2014-07-09 10:20:12 +0800

    用户必须登后才能访问 foods 界面

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,7 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_food, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /foods
   # GET /foods.json
   def index
