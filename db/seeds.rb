271707d68c9640cf0bdccd23d8c4a15e3acc91ea
Peter.Liu <liuhui998@gmail.com>
2014-07-09 15:26:09 +0800

    为了方便测试删除数据，让一份数据存三份

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,12 @@ end
 Food.destroy_all
 User.destroy_all
 
-f1 = Food.create!(:image => food_image_file("hy_orange.jpg"), :name => "汇源果汁", :price => 4, :memo => "好喝")
-f2 = Food.create!(:image => food_image_file("jdb.jpg"), :name => "加多宝", :price => 4, :memo => "来一罐")
-f3 = Food.create!(:image => food_image_file("lf_spring.jpg"), :name => "农夫山泉", :price => 2, :memo => "有点甜")
-f4 = Food.create!(:image => food_image_file("maidong.jpg"), :name => "脉动", :price => 4, :memo => "提提神")
-f5 = Food.create!(:image => food_image_file("pepsi.jpg"), :name => "百式可乐", :price => 3, :memo => "汽水")
+(1..3).each {
+  f1 = Food.create!(:image => food_image_file("hy_orange.jpg"), :name => "汇源果汁", :price => 4, :memo => "好喝")
+  f2 = Food.create!(:image => food_image_file("jdb.jpg"), :name => "加多宝", :price => 4, :memo => "来一罐")
+  f3 = Food.create!(:image => food_image_file("lf_spring.jpg"), :name => "农夫山泉", :price => 2, :memo => "有点甜")
+  f4 = Food.create!(:image => food_image_file("maidong.jpg"), :name => "脉动", :price => 4, :memo => "提提神")
+  f5 = Food.create!(:image => food_image_file("pepsi.jpg"), :name => "百式可乐", :price => 3, :memo => "汽水")
+}
 # 同学们这是 种子测试数据，部署到生产环境后一定要自己改一下
 User.create(email: "test@test.com", password: "12345678")
