1515fc0d9e2bfa810eec4589b22087d8cd16ca68
Peter.Liu <liuhui998@gmail.com>
2014-07-09 10:19:41 +0800

    增加了登录用户的种子数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Food.destroy_all
+User.destroy_all
+
+# 同学们这是 种子测试数据，部署到生产环境后一定要自己改一下
+User.create(email: "test@test.com", password: "12345678")
