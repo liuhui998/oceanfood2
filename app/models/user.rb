4587f67a36e773068b986d105756d69353adb87d
Peter.Liu <liuhui998@gmail.com>
2014-07-09 15:58:04 +0800

    可以显示用户的不同角色，同时关掉了用户注册接口

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
   has_paper_trail 
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable , # :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
   def admin?
     self.role == "admin"
   end
   
+  def show_name
+    "#{self.email} [#{self.role_name}]"
+  end
+  
+  def role_name
+    return "管理员" if admin?
+    return "普通用户"
+  end
 end
