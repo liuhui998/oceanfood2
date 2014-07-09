7e1fa720b8325f384446f6bc6505851ce64a2375
Peter.Liu <liuhui998@gmail.com>
2014-07-09 15:43:51 +0800

    为用户表增加了角色字段

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709060260) do
+ActiveRecord::Schema.define(version: 20140709074316) do
 
   create_table "foods", force: true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140709060260) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
