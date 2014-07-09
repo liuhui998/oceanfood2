e4a6af818329fc29daffe2e145ba4f55b6f3e44d
Peter.Liu <liuhui998@gmail.com>
2014-07-09 11:24:09 +0800

    增加了 foods 相关的测试数据，方便调试

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def food_image_file(name)
+  p = File.join(Rails.root,"test","images","foods",name)
+  File.open(p)
+end
+
 Food.destroy_all
 User.destroy_all
 
+f1 = Food.create!(:image => food_image_file("hy_orange.jpg"), :name => "汇源果汁", :price => 4, :memo => "好喝")
+f2 = Food.create!(:image => food_image_file("jdb.jpg"), :name => "加多宝", :price => 4, :memo => "来一罐")
+f3 = Food.create!(:image => food_image_file("lf_spring.jpg"), :name => "农夫山泉", :price => 2, :memo => "有点甜")
+f4 = Food.create!(:image => food_image_file("maidong.jpg"), :name => "脉动", :price => 4, :memo => "提提神")
+f5 = Food.create!(:image => food_image_file("pepsi.jpg"), :name => "百式可乐", :price => 3, :memo => "汽水")
 # 同学们这是 种子测试数据，部署到生产环境后一定要自己改一下
 User.create(email: "test@test.com", password: "12345678")
